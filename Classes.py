73e058c4edd983fefcc0d230ae00bfc34f93051f
Zorrino <41651765+Zorrino@users.noreply.github.com>
2018-07-25 00:59:57 +0200

    Add files via upload

--- Classes.py
+++ Classes.py
@@ -27,19 +27,6 @@ class Personnage:
         self.classe = classe
         self.nom = nom
 
-    def show(self): #Montre juste les stats, pas le contenu de l'inventaire ni le stuff
-        print("Nom = ",self.nom)
-        print("Pv = ",self.pv )
-        print("Armure = ",self.armure )
-        print("Force = ",self.force )
-        print("Vitesse = ",self.vitesse )
-        print("Critique = ",self.critique,"%")
-        print("Precision = ",self.precision,"%")
-        print("Defense = ",self.defense )
-        print("Prospection = ",self.prospection )
-        print("Classe = ",self.classe )
-        
-
 class Monstre:
     def __init__(self,nom,vitesse,pv,force,loot):
         self.nom = nom
