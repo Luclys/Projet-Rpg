4f2fa75ef16664f668ced527f9597cb9c56ebf35
Zorrino <41651765+Zorrino@users.noreply.github.com>
2018-07-25 01:25:07 +0200

    Add files via upload

--- Classes.py
+++ Classes.py
@@ -7,28 +7,23 @@ from Visualisation import *
 from FonctionImportante import *
 
 
-
 class Personnage:
     inventaire = dict()
-    equippement = list()
-    def __init__(self, vitesse, pv, force, defense, classe, prospection, critique, precision, armure, nom, mana, argent):
-        
+    equipement = list()
+    def __init__(self, nom, pv, force, defense, classe, prospection, critique, precision, armure, vitesse, mana, argent): 
         self.pv = pv
         self.armure  = armure
-        
         self.force = force
         self.vitesse = vitesse
         self.critique = critique
         self.precision = precision
-        
         self.defense = defense
         self.argent = argent
         self.mana = mana
         self.prospection = prospection
-
         self.classe = classe
         self.nom = nom
-
+        
 class Monstre:
     def __init__(self,nom,vitesse,pv,force,loot):
         self.nom = nom
@@ -38,14 +33,14 @@ class Monstre:
         self.loot = loot 
 
 class Effet:
-  	def __init__(self,nom,valeur,duree,description):
-      	self.nom = nom
+    def __init__(self,nom,valeur,duree,description):
+        self.nom = nom
         self.valeur = valeur
         self.duree = duree
         self.description = description
 
 class Item:
-	def __init__(self,nom,cout,rarete,poids,description):
+    def __init__(self,nom,cout,rarete,poids,description):
         self.nom = nom
         self.cout = cout
         self.rarete = rarete
@@ -53,17 +48,17 @@ class Item:
         self.description = description
         
 class Consommable(Item): 
- 	def __init__(self,effet): 
-   	 	self.effet = effet
+    def __init__(self,effet): 
+        self.effet = effet
         
 class Equipement(Item):
- 	def __init__(self,durabilite,armure,effet_sur_joueur,effet_sur_mob,emplacement):
+    def __init__(self,durabilite,armure,effet_sur_joueur,effet_sur_mob,emplacement):
       	self.durabilite = durabilite
-        self.armure = armure
-        self.effet_sur_joueur = effet_sur_joueur
-        self.effet_sur_mob = effet_sur_mob
-        self.emplacement = emplacement
+      	self.armure = armure
+      	self.effet_sur_joueur = effet_sur_joueur
+      	self.effet_sur_mob = effet_sur_mob
+      	self.emplacement = emplacement
       	
 class Arme(Equipement):
-  	def __init__(self,degat):
+    def __init__(self,degat):
       	self.degat = degat    
