c98f04ae5e08f20fef6bf30529e342dee99b65f3
Zorrino <41651765+Zorrino@users.noreply.github.com>
2018-07-25 01:01:52 +0200

    Update Visualisation.py

--- Visualisation.py
+++ Visualisation.py
@@ -10,20 +10,22 @@ from Visualisation import *
 
 def showPerso(perso): #Montre juste les stats, pas le contenu de l'inventaire ni le stuff
     print("Nom = ",perso.nom)
-    print("Pv = ",perso.pv )
-    print("Armure = ",perso.armure )
-    print("Force = ",perso.force )
-    print("Vitesse = ",perso.vitesse )
+    print("Pv = ",perso.pv)
+    print("Mana = ", perso.mana)
+    print("Armure = ",perso.armure)
+    print("Force = ",perso.force)
+    print("Vitesse = ",perso.vitesse)
     print("Critique = ",perso.critique,"%")
     print("Precision = ",perso.precision,"%")
-    print("Defense = ",perso.defense )
-    print("Prospection = ",perso.prospection )
-    print("Classe = ",perso.classe )
+    print("Defense = ",perso.defense)
+    print("Prospection = ",perso.prospection)
+    print("Classe = ",perso.classe)
+    print("Argent = ",perso.argent)
 
 
 def showItem(item):
     print("Nom = ",perso.nom)
     print("Cout = ",perso.cout)
-    print("Rarete = ",perso.rarete )
-    print("Description = ",perso.description )
-    print("Poids = ",perso.poids )
+    print("Rarete = ",perso.rarete)
+    print("Description = ",perso.description)
+    print("Poids = ",perso.poids)
