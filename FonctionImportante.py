41c3092d5daedb63a5a52f0ba7cca535c1d2cec1
Zorrino <41651765+Zorrino@users.noreply.github.com>
2018-07-25 16:20:14 +0200

    Update FonctionImportante.py

--- FonctionImportante.py
+++ FonctionImportante.py
@@ -28,12 +28,12 @@ def supprimer_de_inventaire(item,perso, nb_exemplaire):
 def loot(monstre): #Sous fonction : permet de choisir un item aléatoire dans la liste de drop du monstre.
     return monstre.loot[randint(0,len(monstre.loot) - 1)]
 
-def lootComplet(monstre,joueur): 
+def lootComplet(monstre,perso): 
     listeDeLoot = list()
-    for i in range(0,joueur.prospection): #on loot des item x fois, avec x = prospection du personnage : 1 fois au début et plus après
+    for i in range(0,perso.prospection): #on loot des item x fois, avec x = prospection du personnage : 1 fois au début et plus après
         listeDeLoot.append(loot(monstre))
     for i in listeDeLoot :
-        perso.inventaire[i] += 1 #fonctionne si on considère l'inventaire commme une liste....
+        ajout_dans_inventaire(i,perso,nb_exemplaire):   #Le nombre d'exemplaire n'est pas géré on retravaillera ça
 # !!!!!!!!!!    le i correspond à objet.nom, ça sert à faire le pont avec la "base de donnée" et c'est beaucoup plus facile à traiter
 # (une simple STR) qui est un nom, donc on sait à quoi on a affaire.
 
