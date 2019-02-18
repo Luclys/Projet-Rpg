dea7d9994cee3da47e19efa47089aa81a03b8565
Zorrino <41651765+Zorrino@users.noreply.github.com>
2019-02-18 01:45:54 +0100

    Add files via upload
    Ajout d'un sac à dos ou les objets seront affichés + sa description avec son nom et sa quantité et la fermeture du sac avec un bouton ou directement en retournant au menu principal

--- Lancement.py
+++ Lancement.py
@@ -42,6 +42,9 @@ def changemenu(frame):
     
 inventaire = Frame(fenetre,borderwidth=2,relief=GROOVE, bg="blue")
 inventaire.grid_propagate(0) #Permet de pas redimensionner le frame par rapport aux widgets
+inventaire_objet = Frame(inventaire,borderwidth=2,relief=GROOVE, bg="green", height = 500, width = 500) #Fenetre des items
+inventaire_objet.grid_propagate(0)
+   
 jeu = Frame(fenetre,borderwidth=2,relief=GROOVE, bg="green")
 jeu.grid_propagate(0)
 menu = Frame(fenetre,borderwidth=2,relief=GROOVE, bg="red", height= 700, width = 700)
@@ -56,7 +59,7 @@ jouer = PhotoImage(file='image/oui.png')
 quitte = PhotoImage(file='image/non.png')
 
 
-#boutons menu    
+#MENU DEMARRAGE   
 tojeu = Button(menu, image=jouer, command=partial(changemenu, jeu))
 tojeu.place(x=150, y=150) #permet de placer directement l'element (dans le frame car grid est deja use par le frame lui meme)
 
@@ -64,7 +67,7 @@ tojeu.place(x=150, y=150) #permet de placer directement l'element (dans le frame
 quitter = Button(menu, image=quitte, command= partial(final))
 quitter.place(x=150, y=350)
 
-#boutons jeu
+#MENU PRINCIPAL
 todungeon = Button(jeu, text="Partir à l'aventure !", command=partial(changemenu, menu))
 todungeon.grid()
 
@@ -78,24 +81,46 @@ tomenu = Button(jeu, text="Retour au menu principal", command=partial(changemenu
 tomenu.grid()
 
 
-#boutons inventaire
-tojeu1 = Button(inventaire, text="Retour au menu", command=partial(changemenu, jeu))
-tojeu1.grid(row=0, column=0)
+#MENU INVENTAIRE
 
 
-#Affichage de la description d'un item
+#Ouvre le sac à dos
+def ouvrir_objet():
+    inventaire_objet.grid_propagate(0)
+    inventaire_objet.grid(row = 3, column = 0)
+    ferme_inventaire.grid(row=4, column=0)
+
+#Ferme le sac à dos et la description visible
+def fermer_objet():
+    inventaire_objet.grid_forget()
+    ferme_inventaire.grid_forget()
+    description_cacher()
+
+#On utilise une liste pour "nommer" la frame de description     
 description_item = list()
-def description(nom_obj):
+
+#Vire la frame de description
+def description_cacher():
+    description_item[0].destroy()
+    del description_item[0]
+
+#Affiche la description, le nom de l'objet et sa quantité dans le sac 
+def description_afficher(nom_obj):
     if description_item != list():
         description_item[0].destroy()
         del description_item[0]
-        description_item.append(LabelFrame(inventaire, text="Description", height= 300, width=100))
+        description_item.append(LabelFrame(inventaire, text="Description", height= 300, width=170))
     else :
-        description_item.append(LabelFrame(inventaire, text="Description", height= 300, width=100))
-    description_item[0].grid(row=5, column=8)
-    Label(description_item[0], text=eval(nom_obj).description).grid()
+        description_item.append(LabelFrame(inventaire, text="Description", height= 300, width=170))
+    description_item[0].grid_propagate(0)    
+    description_item[0].grid(row=3, column=1)
+    Label(description_item[0], text=eval(nom_obj).nom).grid(row=0, column = 0)
+    Label(description_item[0], text="Quantité : ").grid(row=1, column = 0)
+    Label(description_item[0], text=Jean.inventaire[eval(nom_obj).nom]).grid(row=1, column = 1) #JEAN DOIT ETRE REMPLACER PAR LE NOM DU PERSO QUE LE JOUEUR CHOISI [A FAIRE]
+    Label(description_item[0], text=eval(nom_obj).description).grid(row=2, column = 0)
+    Button(description_item[0], text="Fermer la description", borderwidth=1, command=partial(description_cacher)).grid(row=5, column=0) #Fermer la description
 
-
+#Ajoute une image dans un dico pour pouvoir l'utiliser plus tard 
 def ajout_img():
     nom_obj = entry_1.get()
     try:
@@ -105,18 +130,29 @@ def ajout_img():
     except IOError:
         print("Erreur! Le fichier n'a pas pu être ouvert")
      
-def ajout_inventaire_et_image():#Permet d'ajouter l'objet dans l'inventaire et decharger son image (et creer un new button si il n'existe pas) 
+def ajout_inventaire_et_image():#Permet d'ajouter l'objet dans l'inventaire et decharger son image sur le bouton(et creer un new button si il n'existe pas) 
     nom_obj = entry_1.get()
-    ajout_dans_inventaire(eval(nom_obj),Jean,1)
+    ajout_dans_inventaire(eval(nom_obj),Jean,1) #JEAN DOIT ETRE REMPLACER PAR LE NOM DU PERSO QUE LE JOUEUR CHOISI [A FAIRE]
     if nom_obj in img_obj:
         return "pas besoin de creer un bouton !"
     else :
         ajout_img()
-        Button(inventaire, image=img_obj[nom_obj], borderwidth=1, command=partial(description, nom_obj)).grid(row=(len(img_obj)+4), column=2)
-    
-dead = Button(inventaire, text="detruire", borderwidth=1).grid(row=0, column=1)
+        Button(inventaire_objet, image=img_obj[nom_obj], borderwidth=1, command=partial(description_afficher, nom_obj)).grid(row=(len(img_obj)+4), column=2)
+
+ferme_inventaire = Button(inventaire, text="Fermer l'inventaire", borderwidth=1, command=partial(fermer_objet))
+ouvre_inventaire = Button(inventaire, text="Ouvrir l'inventaire d'objet", borderwidth=1, command=partial(ouvrir_objet)).grid(row=0, column=1)
+
+#Oblige le sac à dos a se fermer quand on repasse au menu principal
+def backmenu(frame):
+    if description_item != list(): 
+        fermer_objet()
+    changemenu(frame)
 
 
+tojeu1 = Button(inventaire, text="Retour au menu", command=partial(backmenu, jeu)) #retour menu
+tojeu1.grid(row=0, column=0)
+
+#Bouton test pour se give des items
 entry_1 = Entry(inventaire)
 button_1 = Button(inventaire, text="valider", borderwidth=1, command=partial(ajout_inventaire_et_image) ).grid(row=1, column=1)
 entry_1.grid(row=1, column=0)
